d102c831207e76addc97d4e5acd4f6a661acbb12
rcanishan24 <rcanishan24@gmail.com>
2026-05-30 05:38:20 +0800

    Enhance Index component with additional properties

--- app/View/Components/Layout/Index.php
+++ app/View/Components/Layout/Index.php
@@ -9,20 +9,64 @@ use Illuminate\View\Component;
 class Index extends Component
 {
     /**
+     * ROUTING
+     */
+    public string $route;
+    public bool $index;
+    public array $routeParams;
+
+    /**
+     * ✨ UI / UX ENHANCEMENTS
+     */
+    public ?string $title;
+    public ?string $subtitle;
+    public ?string $icon;
+
+    public bool $showCreateButton;
+    public bool $showSearch;
+    public bool $showFilters;
+    public bool $compact;
+    public bool $useCards;
+    public bool $stickyHeader;
+
+    /**
      * Create a new component instance.
      */
     public function __construct(
-        public string $route = '',
-        public bool $index = false,
-        public string $title = '',
-        public array $routeParams = []
-    )
-    {
-        //
+        string $route = '',
+        bool $index = false,
+        string $title = '',
+        array $routeParams = [],
+
+        // ✨ UI ENHANCEMENTS
+        ?string $subtitle = null,
+        ?string $icon = 'list',
+
+        bool $showCreateButton = true,
+        bool $showSearch = true,
+        bool $showFilters = true,
+        bool $compact = false,
+        bool $useCards = true,
+        bool $stickyHeader = true
+    ) {
+        $this->route = $route;
+        $this->index = $index;
+        $this->routeParams = $routeParams;
+
+        $this->title = $title;
+        $this->subtitle = $subtitle;
+        $this->icon = $icon;
+
+        $this->showCreateButton = $showCreateButton;
+        $this->showSearch = $showSearch;
+        $this->showFilters = $showFilters;
+        $this->compact = $compact;
+        $this->useCards = $useCards;
+        $this->stickyHeader = $stickyHeader;
     }
 
     /**
-     * Get the view / contents that represent the component.
+     * Render view
      */
     public function render(): View|Closure|string
     {
